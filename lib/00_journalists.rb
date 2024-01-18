9523889d1e1541524ac2ce7010ea3d5d7fb6e701
splmloic <l2ct@protonmail.com>
2024-01-18 21:27:22 +0100

    L'exercice 1 portant sur le sujet de la gestion des arrays

--- lib/00_journalists.rb
+++ lib/00_journalists.rb
@@ -1,39 +1,95 @@
 journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
-def perform (journalists)
+def exo1 (journalists)
     #Combien y a-t-il de handle dans cette array ?
     puts "Cette liste contient #{journalists.length} journalistes ! ça en fait de la Pige "
-    sleep (1)
+end
+
+def exo2 (journalists)
     #Quel est le handle le plus court de cette liste ? trier par taille puis retourner le [0]
     puts "le journaliste au pseudo le plus court est #{journalists.sort_by { |handle| handle.length}[0] }"
-    sleep (1)
+end
+
+def exo3 (journalists)
     # Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
     puts "#{journalists.count{ |handle| handle.length == 6 }} journalistes ont 5 caractères dans leurs pseudo"
-    sleep (1)
-    # Combien commencent par une majuscule (première lettre juste après le @) ?
-    maj = journalists.map {|handle| handle[1]}.select {|caractere| caractere == caractere.upcase}
-    #je vais détailler la fonction un peu barbare du dessus : je crée une nouvelle array maj qui va stocké uniquement le 2 eme caractére de chaque handle de l'array journalists et garder uniquement ce sont le caractere est en majuscule (upcase)
-    puts "#{maj.length} journalistes ont le pseudo qui commence par une majuscule. Excusez nous les Corpos "
-    sleep (1)
+end
+
+def exo4 (journalists)
+     # Combien commencent par une majuscule (première lettre juste après le @) ?
+    # J'ai galerer la dessus ducoup c'est une nouvelle array qui stock que le 2eme caractère
+     #maj = journalists.map{|handle| handle[1]}
+     #maj = maj.delete_if{ |handle| handle =="_"}
+     #maj = maj.select {|caractere| caractere == caractere.upcase}
+     puts "#{journalists.map{|handle| handle[1]}.delete_if{ |handle| handle =="_"}.select {|caractere| caractere == caractere.upcase}.count} journalistes ont le pseudo qui commence par une majuscule. Excusez nous les Corpos "
+end
+
+def exo5 (journalists)
+    #Trie la liste de handle par ordre alphabétique
+    puts "Maintenant nous allons afficher les utilisateur par ordre alphabétique"
+    puts "#{journalists.sort}"
+    #merci la méthode sort qui sauve bien la mise
+end
+
+def exo6 (journalists)
     #Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
     puts "maintenant nous allons afficher les pseudos du plus petit au plus grand"
-    #puts "#{journalists.sort_by{ |handle| handle.length}}"
+    puts "#{journalists.sort_by{ |handle| handle.length}}"
     #Ya la méthode sort_by et j'ai passer 30 min a faire un algo ...
-    sleep (1)
+end
+
+def exo7 (journalists)
     #Quelle est la position dans l'array de la personne @epenser ?
-    puts "Le célèbre youtubeur @epenser ce trouve en #{journalists.index("@epenser")} places. Il fallait y penser c:"
+    puts "Le célèbre youtubeur @epenser ce trouve en #{journalists.index("@epenser")+1} eme places. Il fallait y penser c:"
     #Donc il faut trouver dans quel case de l'array ce trouve epenser et retourner ce nombre .index va chercher tout ça pour nous
-    sleep (1)
-    #Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+end
+
+def exo8 (journalists)
     i = 0
     until i > 33
         i = i + 1
         if journalists.count{ |handle| handle.length == i} == 0
             else
-                puts "Il y a #{journalists.count{ |handle| handle.length == i}} avec #{i-1} caractère."
+                puts "Il y a #{journalists.count{ |handle| handle.length == i}} journaliste avec #{i-1} caractères.(j'enleve l'@)"
         end
     end
-    #besoin d'une énumération de handle par taille
+        #besoin d'une énumération de handle par taille
+end
+
+def pause
+    sleep(1)
+    stop = ""
+    until stop != "" do
+        puts " "       
+        puts "Pour continuer appuyez sur espace puis Entrée"
+        print "> "
+        stop = gets.chomp.to_s
+    end
+end
+
+def perform (journalists)
+ puts "Bienvenue dans JOURNALISTE ANALYZOR"
+ sleep(1)
+ puts "---INITIALISATION---"
+ sleep(1)
+ puts"voici le dernier rendu du logicel"
+ pause
+ exo1 (journalists)
+ pause
+ exo2 (journalists)
+ pause
+ exo3 (journalists)
+ pause
+ exo4 (journalists)
+ pause
+ exo5 (journalists)
+ pause
+ exo6 (journalists)
+ pause
+ exo7 (journalists)
+ pause
+ exo8 (journalists)
+ pause
 end
 
 perform (journalists)
